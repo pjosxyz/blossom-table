a0547e8700dc9fd8245d616678c42f7b2d666eb5
pjosxyz <pjosxyz@gmail.com>
2024-12-11 22:59:01 +0000

    added toast

--- src/components/layout/base-layout.tsx
+++ src/components/layout/base-layout.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/layout/header";
 import { PropsWithChildren } from "react";
-
+import { Toaster } from "@/components/ui/toaster";
 function BaseLayout({ children }: PropsWithChildren) {
   return (
-    <main className="h-[100dvh] lg:mx-auto lg:px-4 xl:container max-w-6xl">
-      <Header />
-      <div className="mt-10">{children}</div>
-    </main>
+    <>
+      <main className="h-[100dvh] lg:mx-auto lg:px-4 xl:container max-w-6xl">
+        <Header />
+        <div className="mt-10">{children}</div>
+      </main>
+      <Toaster />
+    </>
   );
 }
 
--- src/components/servers-table/components/copy-url-button.tsx
+++ src/components/servers-table/components/copy-url-button.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 import { Copy } from "lucide-react";
 
 export default function CopyURLButton({ url }: { url: string }) {
+  const { toast } = useToast();
+
   function handleCopy() {
     navigator.clipboard.writeText(url);
+    toast({
+      description: "URL copied to clipboard",
+      duration: 2500,
+    });
   }
 
   return (
